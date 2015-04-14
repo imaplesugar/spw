861c3796e48e987fcb36830365c2082b7f45b3ab
imaplesugar <imaplesugar@gmail.com>
2015-04-14 23:39:23 +0700

    *04* Update Space Ship
    change color

--- f2/spw/SpaceShip.java
+++ f2/spw/SpaceShip.java
@@ -14,7 +14,7 @@ public class SpaceShip extends Sprite{
 
 	@Override
 	public void draw(Graphics2D g) {
-		g.setColor(Color.Red);
+		g.setColor(Color.WHITE);
 		g.fillRect(x, y, width, height);
 		
 	}
